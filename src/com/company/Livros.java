e6856f4d461a2b7f1efd601d8a57aa401c289ae2
kimitsu <ks.full.ks@gmail.com>
2021-02-24 14:28:08 -0300

    Biblioteca 1.1_1

---
+++ src/com/company/Livros.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Livros {
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
